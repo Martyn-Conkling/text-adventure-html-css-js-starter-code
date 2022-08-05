b7aebc9aaa8a11fc0646bb343a6d9c60830e0512
Martyn-Conkling <23250543+Martyn-Conkling@users.noreply.github.com>
2022-08-04 19:41:54 -0700

    Last changes from thursday 08-04-22 class

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 let lastCommand = null;
-let commandData = "";
+let commandData = [];
 
-const pCObj = {
+const playerCharObj = {
     time: 0,
     health: 100,
     status: "healthy",
-    possibleStatuses: ["poisoned", "healthy", "dead"]
+    possibleStatuses: ["poisoned", "healthy", "dead"],
+    monsterEncountered: false,
+    items: ["sandwich"],
 
 
 }
 
+
 const commandObject = {
+
+   
+
+    help:{},
+
+    
+
+    inspect: {
+        north: {
+            entrance:{},
+        }
+    },
 
     move: {
         north: {},
         south: {},
         east: {},
         west: {}
-    }
+    },
+
+    take:{},
+
+
+    use:{
+        rock:{},
+        torch:{},
+    },
    
 
 }
@@ -36,7 +59,7 @@ function runCommand(command){
     //we want to look at the commandData Array
     //Look at the first string in the array
     //See if that command is in our commandObject
-    //
+    
     if(commandObject[command] === undefined || commandObject[command] === null){
         // print to the info text: "That is not a valid command"
     }
@@ -47,12 +70,14 @@ function runCommand(command){
 
 function parseCommand(){
     // Set our lastCommand variable to the command
+    // we are using a querry selector to get this data from the DOM
     lastCommand = document.getElementById("user-input").value;
     // console log the value
    console.log( document.getElementById("user-input").value);
 // reset the input to empty
    document.getElementById("user-input").value = "";
 
+//
     commandData = lastCommand.split(" ");
     console.log(commandData);
 
