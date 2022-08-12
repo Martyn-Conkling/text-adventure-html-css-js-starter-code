cd041c7a07ffb5405d333928a0e690b02a2a92b3
Martyn-Conkling <23250543+Martyn-Conkling@users.noreply.github.com>
2022-08-11 19:51:50 -0700

    Added the info-text functionality

--- script.js
+++ script.js
@@ -8,67 +8,6 @@ const playerCharObj = {
 
 }
 
-const gameState = {
-    currentRoomID: 0,
-    currentRoomObject: gameMap[0],
-    previousCommand: null,
-    currentCommand: null,
-    commandData: [],
-    currentDescription: null,
-
-
-    monsterEncountered: false,
-    time: 0,
-    adventureLogArray: [
-        "You open your eyes.",
-        "You are in a dim cave lying on the ground.",
-        "Your head aches with a dull pain",
-        "Light is streaming in from a hole in the cave ceiling above you."
-         ]
-
-
-};
-
-
-
-const commandObject = {
-
-   
-
-    help:{ printHelp(){
-
-
-        }
-    },
-
-    
-
-    inspect: {
-        north: {
-            entrance:{},
-        }
-    },
-
-    move: {
-        north: {},
-        south: {},
-        east: {},
-        west: {}
-    },
-
-    take:{
-
-    },
-
-
-    use:{
-        rock:{},
-        torch:{},
-    },
-   
-
-}
-
 const gameMap = {
 
     0: { roomDescription:[
@@ -94,62 +33,168 @@ const gameMap = {
     4: {},
 }
 
-function runCommand(command, object){
+const gameState = {
+    currentRoomID: 0,
+    currentRoomObject: gameMap[0],
+    previousCommand: null,
+    currentCommand: null,
+    commandArray: [],
+    currentDescription: null,
 
-    //we want to look at the commandData Array
+
+    monsterEncountered: false,
+    time: 0,
+    adventureLogArray: [
+        "You open your eyes.",
+        "You are in a dim cave lying on the ground.",
+        "Your head aches with a dull pain",
+        "Light is streaming in from a hole in the cave ceiling above you."
+         ]
+
+
+};
+
+
+
+const commandObject = {
+
+   
+
+    help:{ 
+        "text": ["Help function works", "Help function works","Help function works" ]
+        },
+
+    
+
+    inspect: {
+        "text": ["Inspect function works", "Inspect function works", "Inspect function works"],
+        north: {
+            entrance:{},
+        }
+    },
+
+    move: {
+        "text": ["Move function works", "Move function works", "Move function works",],
+
+        north: {},
+        south: {},
+        east: {},
+        west: {}
+    },
+
+    take:{
+        "text": ["take function works", "take function works", "take function works"],
+
+    },
+
+
+    use:{
+        "text": ["use function works", "use function works", "use function works",],
+        rock:{},
+        torch:{},
+    },
+   
+
+}
+
+
+
+function runValidCommand(command, object){
+
+    //we want to look at the commandArrayArray
     //Look at the first string in the array
     //See if that command is in our commandObject
     
     if(object[command] === undefined || commandObject[command] === null){
         // print to the info text: "That is not a valid command"
+
+        return ["This is not a valid command"];
+    }
+    else if(object[command]){
+        return object[command]["text"];
     }
 
 
 
 };
 
-function parseCommand(){
+function evaluateCommandArray(){
+
+}
+
+// This function gets the command the user wrote in our input
+function parseUserCommand(){
+
     //We set our previousCommand property to be the command that is currently our currentCommand
     gameState.previousCommand = gameState.currentCommand
 
     // we update our currentCommand property to the command that the user just submitted from the input
     // we are using a querry selector to get this data from the DOM
     gameState.currentCommand = document.getElementById("user-input").value;
-
+    
     // console log the value for debugging purposes
-   console.log( document.getElementById("user-input").value);
+   console.log( gameState.currentCommand);
 
     // reset the input to empty
    document.getElementById("user-input").value = "";
 
 //
-    gameState.commandData = gameState.currentCommand.split(" ");
-    console.log(commandData);
+    gameState.commandArray = gameState.currentCommand.toLowerCase().split(" ")
+    
+    console.log("commandArray is currently: ", gameState.commandArray);
+
+}
+
+function printTextToInfoTextSection(textArray){
+    // Loop through the textArray we are given
+    //We will print each line of text, which has been saved as an element in an array
+    let infoText = document.getElementById("info-text");
+
+    let currentCommand = document.createElement("p");
+    let currentCommandText = document.createTextNode( "You wrote: " + gameState.currentCommand)
+    currentCommand.appendChild(currentCommandText);
+    infoText.appendChild(currentCommand);
+    
+        for(let i = 0; i < textArray.length; i++){
+
+            let newLineOfText = document.createElement("p");
+            let x = document.createTextNode(textArray[i]);
+            newLineOfText.appendChild(x);
+
+            infoText.appendChild(newLineOfText);
+    
+        };
+
+   let promptText = document.createTextNode("What do you want to do?") 
+   let prompt = document.createElement("p")
+        prompt.appendChild(promptText);
+        infoText.appendChild(prompt);
+
+    
+}
+
+function scrollToBottomOfElementByIDName(idOfElement){
+    let element = document.getElementById(idOfElement);
+    element.scrollTop = element.scrollHeight;
+
+}
+
+function parseAndExecuteUserInput(){
+
+    parseUserCommand();
+
+    printTextToInfoTextSection(runValidCommand(gameState.commandArray[0], commandObject))
+    
+    // runValidCommand()
+    
+    scrollToBottomOfElementByIDName("info-text");
 
 }
 
 
-function printTextToConsole(textArray){
-// Loop through the textArray we are given
-//We will print each line of text, which has been saved as an element in an array
-    for(let i = 0; i < textArray.length; i++){
-
-        document.getElementById("info-text").
-
-    }
 
 
-}
 
-// The code for printing description text and feedback text to the info-section box
-
-// each time you enter a new command it should print out the command on a new line
-// All text data displayed in this section should be in an array, 
-//Then in the info-section, you should print the command the user inputed, 
-//Print the feedback message 
-    // if unsuccessful print the unsuccessful message
-    // if it was successful you print the feedback message for whatever action you did
-    // if there were any changes to the game state that triggered any flags
 
 
 
